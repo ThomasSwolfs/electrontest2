1ab016f45416a0a24abc3a4ea63f29ef03b6cc67
swolfth <swolfth@cronos.be>
2023-10-13 20:54:02 +0200

    linux test

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ const path = require('node:path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  //  width: 800,
+   // height: 600,
+    fullscreen: true,
+    kiosk: true,
+    autoHideMenuBar : true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
